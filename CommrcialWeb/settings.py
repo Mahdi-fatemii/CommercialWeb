245fa21d364233f9274a57cf8b24a631a077b7a9
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2024-01-10 12:27:41 +0330

    register/login/logout

--- CommrcialWeb/settings.py
+++ CommrcialWeb/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = "user.CustomUser"
 
+LOGIN_URL = 'login'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import CustomUserCreationForm
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -11,12 +13,23 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome {username}.')
+            # username = form.cleaned_data.get('email')
+            # messages.success(request, f'Welcome {username}.')
             return render(request, 'user/login.html')
     else:
         form = CustomUserCreationForm()
     return render(request, 'user/register.html', {'form': form})
+
+
+class MyLoginView(LoginView):
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('profile')
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 @login_required
